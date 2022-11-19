61dce91e4bc16b03489452ad765e52b994bb7059
Julian Bildner <julian.bildner@leanio.de>
2022-11-19 12:32:52 +0100

    Schuelerstimmen und Hörprobe

--- musik-passau/ViewModels/Schuelerstimme.cs
+++ musik-passau/ViewModels/Schuelerstimme.cs
@@ -2,6 +2,19 @@ namespace ViewModels;
 
 public class Schuelerstimme
 {
+    public Schuelerstimme(string titel)
+    {
+        this.titel = titel;
+    }
+
+    public Schuelerstimme()
+    {
+    }
+
+    private string? titel;
+    public string Ueberschrifft => titel ??
+                                   $"{Name}, {(int) ((DateTime.Now - Geburtstag).Days / 365.25)}, {(SchuelerTyp == SchuelerTyp.Gesang ? "Gesangsschüler" : "Klavierschüler")}{(Maennlich ? "" : "in")} seit {SchuelerSeit}";
+    
     public string Name { get; set; }
     public DateTime Geburtstag { get; set; }
     public SchuelerTyp SchuelerTyp { get; set; }
